d607573d79ddb9739cd92d0451cf02e5e7ea9358
Andrew <andrew.j.t.p@gmail.com>
2020-12-01 09:18:22 -0400

    precio base

--- template2_6_5-master/lib/test.rb
+++ template2_6_5-master/lib/test.rb
@@ -1,5 +1,10 @@
 class Test
     def sumar(n1,n2)
-        res=n3+n2
+        res=n1+n2
+    end
+    #def calcular_precio_final(cantidad_items, precio_unitario, estado)
+    #end
+    def calcular_precio_base(cantidad_items,precio_unitario)
+        precio=cantidad_items*precio_unitario
     end
 end
--- template2_6_5-master/spec/test_spec.rb
+++ template2_6_5-master/spec/test_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Test do
     it 'debería devolver 7 la suma de 3 + 4' do
        expect(@test.sumar(3, 4)).to eq(7)
     end
-
+    it 'debería devolver 15, 5 productos que cuestan 3bs' do
+        expect(@test.calcular_precio_base(5, 3)).to eq(15)
+     end
 end
